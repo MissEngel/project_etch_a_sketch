b62aa3383bb8753ead8bdf4dd096612e9bb86aba
Jesus Silveira <silveirajesus69@gmail.com>
2024-06-08 13:33:17 -0400

    finish grid and buttons logic

--- script.js
+++ script.js
@@ -1,32 +1,57 @@
 const squareContainer = document.querySelector("#square_container");
 
-for(let i = 1; i <= 16; i++){
+function createGrid(squaresNumber){
 
-    const div = document.createElement("div");
-    div.setAttribute("id", `div${i}`);
-    squareContainer.appendChild(div);
-    document.querySelector(`#div${i}`).style.display = "flex";
-    document.querySelector(`#div${i}`).style.flexDirection = "column";
-    document.querySelector(`#div${i}`).style.flexGrow = "1";
-    document.querySelector(`#div${i}`).style.flexShrink = "1";
+    squareContainer.innerHTML = '';
 
-    for(let j = 1; j <= 16; j++){
-        const div2 = document.createElement("div");
-        div2.setAttribute("id", `div${i}_${j}`);
-        div.appendChild(div2);
-        document.querySelector(`#div${i}_${j}`).style.flexBasis = "3.5rem";
-        document.querySelector(`#div${i}_${j}`).style.border = "1px solid gray";
-        document.querySelector(`#div${i}_${j}`).style.backgroundColor = "white";
-        document.querySelector(`#div${i}_${j}`).style.flexGrow = "1";
-        document.querySelector(`#div${i}_${j}`).style.flexShrink = "1";
+    for(let i = 1; i <= squaresNumber; i++){
+    
+        const div = document.createElement("div");
+        div.setAttribute("id", `div ${i}`);
+        div.style.display = "flex";
+        div.style.flexDirection = "column";
+        div.style.flexGrow = "1";
+        div.style.flexShrink = "1";
+        squareContainer.appendChild(div);
+    
+        for(let j = 1; j <= squaresNumber; j++){
+            const div2 = document.createElement("div");
+            div2.setAttribute("id", `square`);
+            div2.style.flexBasis = "3.5rem";
+            div2.style.border = "1px solid gray";
+            div2.style.backgroundColor = "white";
+            div2.style.flexGrow = "1";
+            div2.style.flexShrink = "1";
+    
+            div2.addEventListener("mouseover", () => {
+                div2.style.backgroundColor = "black";
+            });
 
-        div2.addEventListener("mouseover", () => {
-            div2.style.backgroundColor = "black";
-        });
+            div.appendChild(div2);
+        }
     }
 }
 
-function createGrid(squaresNumber){
+createGrid(16);
 
-}
+const cleanButton = document.querySelector("#clean_button");
 
+cleanButton.addEventListener("click", () => {
+    const squares = document.querySelectorAll("#square");
+    squares.forEach(square => {
+        square.style.backgroundColor = "white";
+    });
+})
+
+const newButton = document.querySelector("#create_button");
+
+newButton.addEventListener("click", () => {
+
+    let desiredSquares;
+
+    while(!(desiredSquares <= 100)){
+        desiredSquares = +(prompt("How many squares would you like this etch a sketch be? (no more than 100)"));
+    }
+    
+    createGrid(desiredSquares);
+})
